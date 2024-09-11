7b60da83daaa7f23837a9b3d425bf1c6829f9916
swatiyad <wati.nadcabtechnology@gmail.com>
2024-09-11 17:05:56 +0530

    after correction

--- src/jsx/components/Users/ApexChart.js
+++ src/jsx/components/Users/ApexChart.js
@@ -31,11 +31,11 @@ const ApexChart = () => {
 
   const [chartSeries, setChartSeries] = useState([
     {
-      name: 'Collateral', // Name of the first series
+      name: 'Deposit', // Name of the first series
       data: [30, 40, 35, 50, 49, 60], // Data points for the "Collateral" series
     },
     {
-      name: 'Acquisition', // Name of the second series
+      name: 'Withdrawal', // Name of the second series
       data: [20, 30, 45, 60, 49, 70], // Data points for the "Acquisition" series
     },
   ]);
